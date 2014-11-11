f614d07e07cd07e2ff893b3a2be42cef74d75ec5
Jason <jayo2794@gmail.com>
2014-11-11 12:22:13 +0000

    Getting started

--- src/ie/itcarlow/CastleHell/Main.java
+++ src/ie/itcarlow/CastleHell/Main.java
@@ -1,5 +1,76 @@
 package ie.itcarlow.CastleHell;
 
-public class Main {
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.ui.IGameInterface.OnCreateResourcesCallback;
+import org.andengine.ui.IGameInterface.OnCreateSceneCallback;
+import org.andengine.ui.activity.BaseGameActivity;
 
+public class Main extends BaseGameActivity implements IUpdateHandler {
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+	
+	private Scene mScene;
+	
+	private BitmapTextureAtlas playerAnimatedSprite;
+	private ITiledTextureRegion playerTiledTextureRegion;
+	AnimatedSprite sprNum;
+	
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+	}
+    @Override
+	public void onCreateResources(
+       OnCreateResourcesCallback pOnCreateResourcesCallback)
+			throws Exception {
+
+    	 loadGfx();
+		 pOnCreateResourcesCallback.onCreateResourcesFinished();
+
+    }
+
+    private void loadGfx() {     
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");  
+
+        
+    }
+
+    @Override
+  	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+  			throws Exception {
+
+  		this.mScene = new Scene();
+  		this.mScene.setBackground(new Background(0, 125, 58));
+  		//register this activity as a scene touch listener
+  		//this.mScene.setOnSceneTouchListener(this);
+  	    pOnCreateSceneCallback.onCreateSceneFinished(this.mScene);  		
+  	}
+	@Override
+	public void onPopulateScene(Scene pScene,
+			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onUpdate(float pSecondsElapsed) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
 }
