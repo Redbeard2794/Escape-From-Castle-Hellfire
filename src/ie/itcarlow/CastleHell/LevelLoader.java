a39e1ce6cfc8e27e65cc19b6e18464556c4d82c7
Jason <jayo2794@gmail.com>
2014-11-25 13:02:48 +0000

    Level loader started, some sprite sheet fiddling

---
+++ src/ie/itcarlow/CastleHell/LevelLoader.java
@@ -0,0 +1,13 @@
+package ie.itcarlow.CastleHell;
+
+public class LevelLoader {
+	private static final String TAG_ENTITY = "entity";
+	private static final String TAG_ENTITY_ATTRIBUTE_X = "x";
+	private static final String TAG_ENTITY_ATTRIBUTE_Y = "y";
+	private static final String TAG_ENTITY_ATTRIBUTE_WIDTH = "width";
+	private static final String TAG_ENTITY_ATTRIBUTE_HEIGHT = "height";
+	private static final String TAG_ENTITY_ATTRIBUTE_TYPE = "type";
+	
+	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PLATFORM = "platform";
+	private static final Object TAG_ENTITY_ATTRIBUTE_TYPE_VALUE_PROXIMITYTRAP = "proximityTrap";
+}
